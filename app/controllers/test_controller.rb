28a0ae5991dabdcd6857bfc53bc93c0fcd72af48
iarch <rodriguezch_ignacioandres@hotmail.cl>
2017-04-05 10:31:53 -0300

    modificaciones vista y controller test

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,7 @@
 class TestController < ApplicationController
+  protect_from_forgery with: :exception
   def index
+    Rails.logger.info 'Entro testcontroller --------'
+    @date = DateTime.now
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'test#index'
+  root to: 'test#index' # Hacemos esto para definir la primera página; siempre debe ir al principio
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
