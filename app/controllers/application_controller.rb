0de2f5a74ea32e0e5b7495854e749b4b97f26835
iarch <rodriguezch_ignacioandres@hotmail.cl>
2017-04-04 21:16:55 -0300

    cambio primera vista y foreman con procfile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def metodo_asd
-    puts 'asdasd'
-  end
-
-  private
-  def asdas
-    puts "asd"
-  end
-
-  def asdsdsadas
-    puts "hola"
-  end
-  
-  
+  Rails.logger.info "ENTRO A APPLICATION CONTROLLER -------------"  
 end
